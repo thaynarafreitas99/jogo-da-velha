32cfd9fb045effa87e4ff1e63232786c3bec4710
Thaynara Silva <thaynara.silva@abcfitness.com>
2023-09-03 11:52:29 -0300

    jogo finalizado

--- estilos.js
+++ estilos.js
@@ -1,16 +1,31 @@
 var table = document.getElementById("table");
+var containerPlay = document.getElementById("container-play");
+
 table.style.display = 'none'
+containerPlay.style.display = 'none'
 
 var blocoInicial = document.getElementById("bloco-inicial");
 blocoInicial.style.display = 'block'
 
-const jorgador1 = document.getElementById('jogador1');
-const jorgador2 = document.getElementById('jogador2');
+var jorgador1 = document.getElementById('jogador1');
+var jorgador2 = document.getElementById('jogador2');
+
+var jogador1Selecionado = document.getElementById('jogador1Selecionado');
+var jogador2Selecionado = document.getElementById('jogador2Selecionado');
 
 
 var botaoAdicionar = document.querySelector("#botao-iniciar-jogo");
 
 botaoAdicionar.addEventListener("click", function(){
-    blocoInicial.style.display = 'none'
-    table.style.display = 'block'
+    debugger;
+    if(jorgador1.value == '' || jorgador2.value == ''){
+        alert('Preencha o nome dos jogadores para continuar');
+    }else{
+        blocoInicial.style.display = 'none'
+        table.style.display = 'block'
+        containerPlay.style.display = 'flex'
+        jogador1Selecionado.innerText = jorgador1.value;
+        jogador2Selecionado.innerText = jorgador2.value;
+    }
+
 });
--- main.js
+++ main.js
@@ -1,10 +1,16 @@
+var jorgador1 = document.getElementById('jogador1');
+var jorgador2 = document.getElementById('jogador2');
+
+var containerJogadorUm = document.getElementById('div-1-selecionado');
+var containerJogadorDois = document.getElementById('div-2-selecionado');
+var jogador1Selecionado = document.getElementById('jogador1Selecionado');
+var jogador2Selecionado = document.getElementById('jogador2Selecionado');
+
+
 // Quando todas as tds forem clicadas
 var todasTds = document.querySelectorAll('td');
-var todasLinhas = document.querySelectorAll('tr');
-
 var combinacaoBolinha = [];
 var combinacaoX = [];
-
 var clicou = 1;
 
 var nomesPorExtenso = {
@@ -62,11 +68,11 @@ todasTds.forEach(function(td) {
         // impar circulo, par x.
         if(clicou % 2 == 0){
             span.className = 'circulo';
-            tabuleiro[posicaoTr][posicaoTd] = true;
+            tabuleiro[posicaoTr][posicaoTd] = false;
         }else{
             span.className = 'x';
             span.textContent = 'X';
-            tabuleiro[posicaoTr][posicaoTd] = false;
+            tabuleiro[posicaoTr][posicaoTd] = true;
         }
 
         tdSelecionada.appendChild(span);
@@ -80,26 +86,38 @@ todasTds.forEach(function(td) {
 
 function verificaPosicoes(){
     for (const sequencia of posicoesGanhadoras) {
+        // Só faz a validação de o tamanho dos array de combinação for menor que 3
         if(combinacaoBolinha.length <= 2 || combinacaoX.length <= 2){
+            // Sempre reseta, pois o looping só é feito 1 matriz (com 3 combinações dentro) por vez
             combinacaoBolinha = [];
             combinacaoX = [];
 
+            jorgador1
+            jogador2
+
+            // looping em cada linha de matriz
             sequencia.forEach(item => {
+                // verifica se a linha e coluna da vez está true, se sim da push no array de bolinha
                 if(tabuleiro[item[0]][item[1]] == true){
                     combinacaoBolinha.push(true);
                     if(combinacaoBolinha.length == 3){
+                        containerJogadorUm.classList.add("x-venceu");
+                        jogador1Selecionado.textContent = jogador1.value + " " +  "Venceu!";
+
                         setTimeout(function() {
-                            return alert('BOLINHA VENCEU!!!')
-                        }, 100);
-                        resetarJogo();
+                            resetarJogo();
+                        }, 4000);
                     }
+                    // se não ele verifica se a posição é false, para dar push no array de x.
                 }else if(tabuleiro[item[0]][item[1]] == false){
                     combinacaoX.push(false);
                     if(combinacaoX.length == 3){
+                        containerJogadorDois.classList.add("bolinha-venceu");
+                        jogador2Selecionado.textContent = jogador2.value + " " + "Venceu!";                  
+
                         setTimeout(function() {
-                            return alert('X VENCEU!!!');
-                        }, 100);
-                        resetarJogo();
+                            resetarJogo();      
+                        }, 4000);                     
                     }
                 }
             });
@@ -107,11 +125,16 @@ function verificaPosicoes(){
         }
     }
 
+    // Se a quantidade de click é 9 e os array for menor que 3 (ninguém venceu, então é velha.)
     if(clicou == 9 && combinacaoX.length < 3 && combinacaoBolinha.length < 3){
+        containerJogadorUm.classList.add("x-venceu");
+        containerJogadorDois.classList.add("bolinha-venceu");
+        jogador1Selecionado.textContent = "Deu velha!";
+        jogador2Selecionado.textContent = "Deu velha!";                  
+
         setTimeout(function() {
-            return alert('DEU VELHA!!!');
-        }, 100);
-        resetarJogo();
+            resetarJogo();      
+        }, 4000);   
     }
 }
 
@@ -119,6 +142,11 @@ function verificaPosicoes(){
 function resetarJogo(){
     clicou = 0;
     combinacaoBolinha = [];
+    containerJogadorUm.classList.remove("x-venceu");
+    containerJogadorDois.classList.remove("bolinha-venceu");
+    jogador1Selecionado.textContent = jogador1.value;
+    jogador2Selecionado.textContent = jogador2.value;                        
+
     combinacaoX = [];
     tabuleiro = [
         [1, 2, 3],
@@ -133,5 +161,4 @@ function resetarJogo(){
 
     });
 }
-
 
